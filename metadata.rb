68e667991533df8f85c92a5af32c16fbae8b2e4c
Brandon Sharitt <bsharitt@franklinamerican.com>
2016-03-30 15:36:44 -0500

    first

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'tomcat8'
+maintainer       'Franklin American Mortgage Company'
+maintainer_email 'bsharitt@franklinamerican.com'
+license          'All rights reserved'
+description      'Installs/Configures tomcat8'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'java'
