0dcf1803b38a3d4b6ca60ab7548b934a1e42b295
Brandon Sharitt <bsharitt@franklinamerican.com>
2016-03-31 13:18:14 -0500

    Add supports ubuntu

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 issues_url       'https://github.com/bsharitt/tomcat8/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/bsharitt/tomcat8' if respond_to?(:source_url)
+supports         'ubuntu'
 depends          'java'
