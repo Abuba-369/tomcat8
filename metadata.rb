6b6cfe1a65ae27ced88132db4bf73ea26b702c54
Brandon Sharitt <bsharitt@franklinamerican.com>
2016-03-31 13:14:13 -0500

    Added issues and source url

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures tomcat8'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+issues_url       'https://github.com/bsharitt/tomcat8/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/bsharitt/tomcat8' if respond_to?(:source_url)
 depends          'java'
